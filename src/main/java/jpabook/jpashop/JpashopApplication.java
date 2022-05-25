13015d50f77c4ae47cd219be353e006d453ecfc3
kod378 <kod378@naver.com>
2022-05-26 00:38:28 +0900

    인프런 강의 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 프로젝트 환경설정 - JPA와 DB 설정, 동작확인 까지

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+//		Hello hello = new Hello();
+//		hello.setData("awdawd");
+//		System.out.println("hello = " + hello.getData());
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
